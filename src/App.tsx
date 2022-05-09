f4ff90b0c2ef85af00b940f1288eacf8cf27a459
levchenki <danila.levchenki@gmail.com>
2022-05-09 17:26:13 +0300

    Not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import BasicLayout from './layout/BasicLayout/BasicLayout'
+import BasicLayout from './Layout/BasicLayout/BasicLayout'
+import NotFoundPage from './Layout/NotFoundPage/NotFoundPage'
 
-const App = () => (
+const App: React.FC = () => (
   <>
     <Routes>
       <Route element={<BasicLayout/>} path='/students'>
       </Route>
-      <Route element={<Navigate to='/students'/>} path='*'/>
+      {/*<Route element={<Navigate to='/students'/>} path='*'/>*/}
+      <Route element={<NotFoundPage/>} path='*'/>
     </Routes>
   </>
 )
---
+++ src/Layout/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './NotFoundPage.module.scss'
+
+const NotFoundPage: React.FC = () => (
+  <div className={style.NotFoundPage}>
+    <h1 className={'_icon-ads'}>Error 404</h1>
+  </div>
+)
+
+export default NotFoundPage
