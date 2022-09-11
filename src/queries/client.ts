cc32d95e30c2e581684d3c5487cf354ff5313db9
KoJem9Ka <bis21080000@gmail.com>
2022-09-11 19:38:27 +0300

    Refresh token functional complete

--- src/queries/client.ts
+++ src/queries/client.ts
@@ -2,33 +2,39 @@ import {
   ApolloClient,
   ApolloLink,
   from,
+  fromPromise,
   HttpLink,
-  InMemoryCache
+  InMemoryCache,
 }                       from '@apollo/client'
 import { onError }      from '@apollo/client/link/error'
 import { AbsolutePath } from '../other/PathConfig'
-import { store }        from '../store/store'
-import { actionLogout } from '../store/globalActions'
 import {
   GLoginInput,
   GLoginMutationFn,
   GRenewTokenMutationFn,
-  LoginDocument
+  LoginDocument,
+  RenewTokenDocument,
 }                       from '../other/generated'
 import { toast }        from 'react-toastify'
 import { IS_DEV }       from '../other/helpers'
+import { store }        from '../store/store'
+import { actionLogout } from '../store/globalActions'
 
 
-//TODO: Нужно доделать рефреш токен
+
 export const Application = {
   async login( { email, password }: GLoginInput ) {
     const startFetch = (client.mutate as GLoginMutationFn)( { mutation: LoginDocument, variables: { user: { email, password } } } )
-    const { data } = await toast.promise( startFetch, { pending: 'Авторизация...', error: 'Ошибка запроса авторизации' } )
+    const { data }   = await toast.promise( startFetch, {
+      pending : 'Авторизация...',
+      error   : 'Ошибка запроса авторизации',
+    } )
     if ( !data?.login ) {
-      toast.error( 'Ошибка запроса авторизации' )
+      toast.error( 'Ошибка в ответе запроса авторизации' )
       return
     }
     if ( data.login.message === 'Success' && data.login.accessToken && data.login.refreshToken ) {
+      toast.success( 'Добро пожаловать!' )
       localStorage.setItem( 'AccessToken', data.login.accessToken )
       localStorage.setItem( 'RefreshToken', data.login.refreshToken )
       document.location.href = AbsolutePath.Root
@@ -44,21 +50,26 @@ export const Application = {
     store.dispatch( actionLogout() )
     document.location.href = AbsolutePath.Login
   },
-  async renewToken(): Promise<null | string> {
-    const accessToken = localStorage.getItem( 'AccessToken' )
+  async renewToken(): Promise<string | void> {
+    const accessToken  = localStorage.getItem( 'AccessToken' )
     const refreshToken = localStorage.getItem( 'RefreshToken' )
-    if ( !accessToken || !refreshToken ) return null
-    const startFetch = (client.mutate as GRenewTokenMutationFn)( { mutation: LoginDocument, variables: { tokens: { accessToken, refreshToken } } } )
-    const { data } = await toast.promise( startFetch, { pending: 'Обновление токена...', error: 'Ошибка запроса обновления токена' } )
+    if ( !accessToken || !refreshToken ) return
+    const startFetch = (client.mutate as GRenewTokenMutationFn)( { mutation: RenewTokenDocument, variables: { tokens: { accessToken, refreshToken } } } )
+    const { data }   = await toast.promise( startFetch, {
+      pending : 'Обновление токена...',
+      error   : 'Ошибка запроса обновления токена',
+    } )
     if ( !data?.relogin.accessToken || !data.relogin.refreshToken ) {
-      toast.error( 'Ошибка запроса обновления токена 123123' )
-      return null
+      toast.error( 'Ошибка в ответе запроса обновления токена' )
+      return
     }
+    toast.success( 'Рефреш токен обновлён!' )
     localStorage.setItem( 'AccessToken', data.relogin.accessToken )
     localStorage.setItem( 'RefreshToken', data.relogin.refreshToken )
     return data.relogin.accessToken
   },
 }
+
 
 const serverDomainLink = new HttpLink( { uri: 'https://localhost:7094' } )
 
@@ -68,24 +79,41 @@ const authMiddleware = new ApolloLink( ( operation, forward ) => {
   return forward( operation )
 } )
 
+let refreshPromise: ReturnType<typeof Application.renewToken> | undefined
 const errorMiddleware = onError( ( { graphQLErrors, networkError, operation, forward } ) => {
+  networkError && toast.error( `Ошибка: ${networkError}`, { autoClose: false } )
   if ( !graphQLErrors ) return
-  graphQLErrors.forEach( err => {
-    console.error( 'graphQLError:', err )
-    err.extensions.code === 'AUTH_NOT_AUTHENTICATED' && Application.renewToken().then( token => {
-      if ( token === null ) return Application.logout()
-      const oldHeaders = operation.getContext().headers
-      operation.setContext( { headers: { ...oldHeaders, authorization: token ? `Bearer ${token}` : '' } } )
-      return forward( operation )
-    } ).catch( () => Application.logout() )
-  } )
-  networkError && console.error( 'networkError:', networkError )
+  for ( const err1 of graphQLErrors ) {
+    if ( err1.extensions.code === 'AUTH_NOT_AUTHENTICATED' ) {
+      console.log( 'error' )
+      return fromPromise(
+        refreshPromise || (refreshPromise = Application
+            .renewToken()
+            .then( token => {
+              refreshPromise = undefined
+              return token
+            } )
+            .catch( error => Application.logout() ))
+      )
+          .filter( value => Boolean( value ) )
+          .flatMap( token => {
+            const oldHeaders = operation.getContext().headers
+            operation.setContext( {
+              headers : {
+                ...oldHeaders,
+                authorization : `Bearer ${token}`,
+              },
+            } )
+            return forward( operation )
+          } )
+    }
+  }
 } )
 
 export const client = new ApolloClient( {
-  cache:             new InMemoryCache(),
-  connectToDevTools: IS_DEV,
-  link:              from( [
+  cache             : new InMemoryCache(),
+  connectToDevTools : IS_DEV,
+  link              : from( [
     errorMiddleware,
     authMiddleware,
     serverDomainLink
